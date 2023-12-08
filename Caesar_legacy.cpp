e4b54ddcf5079ba34d97061b50df17ad87f771e8
bellmann-engineering <57789413+bellmann-engineering@users.noreply.github.com>
2023-12-08 08:08:17 +0100

    Update Caesar_legacy.cpp

--- Caesar_legacy.cpp
+++ Caesar_legacy.cpp
@@ -3,13 +3,43 @@
 
 using namespace std;
 
-
 int main()
-{	
-	int shift;
+{
+	int shift = 0;
+	
+	cin.exceptions(ios_base::failbit);
+	
+	cout << "Zahl eingeben:" ;		
+	
+	try
+	{	
+		cin >> shift;
+	}
+	/*catch(ios_base::failure&)
+	{
+		cout << "IOS Exception" << endl;
+		return 0;
+	}*/	
+	catch(exception& e)
+	{
+		cout << "Laufzeitfehler: "  << e.what() << endl;
+		return 0;
+	}
+	catch(...)
+	{
+		cout << "alle Laufzeitfehler" << endl;
+		return 0;
+	}
+	
+	/*if(cin.fail())
+	{
+		cout << "das war keine zahl" << endl;
+		cin.clear();
+		cin.ignore();
 		
-	cout << "Zahl fuer Shifting eingeben: ";
-	cin >> shift;
+		return 0;
+	}*/
+	
 
 	
 	CaesarExtended c("montag", shift);
@@ -17,6 +47,4 @@ int main()
 	string encryptedSentence = c.encrypt(sentence);
 	cout << "Verschuesselt: " << encryptedSentence << endl;
 	cout << "Entschuesselt: " << c.decrypt(encryptedSentence) << endl;
-	
-	return 0;
 }
